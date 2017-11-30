068cdd98254f02ddf88a86c26bfa0cf6da9e1d01
jamesvrooney <jamesvrooney@hotmail.com>
2017-11-30 09:33:04 +0000

    Added project for Adapter Design Pattern

---
+++ structural/adapter-design-pattern/src/main/java/com/rooney/james/adapterdesignpattern/AdapterDesignPatternApplication.java
@@ -0,0 +1,12 @@
+package com.rooney.james.adapterdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdapterDesignPatternApplication.class, args);
+	}
+}
